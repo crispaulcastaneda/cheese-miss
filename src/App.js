cd1190ea57b0d5625469e75dd070c8d5cce880f5
Cristian Paul Castañeda <cristianpaul_c@yahoo.com>
2025-08-07 20:53:15 +0800

    Add routes, hooks, themes, data, layouts, utilities

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Router from "./routes";
+import ThemeProvider from './theme';
+import ThemeSettings from './components/settings';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <ThemeSettings>
+        {" "}
+        <Router />{" "}
+      </ThemeSettings>
+    </ThemeProvider>
+  );
+}
+
+export default App;

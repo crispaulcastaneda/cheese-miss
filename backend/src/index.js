2ed7484bcc956e0ee87d9f73cc451e98f06a8ffa
Cristian Paul Castañeda <cristianpaul_c@yahoo.com>
2025-09-26 23:14:33 +0800

    Modified backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
@@ -12,8 +15,13 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json()); // extract JSON data from requests
 app.use(cookieParser()); // parse cookies from requests
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port:" + PORT);

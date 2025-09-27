7983e810df0caf46eaf7ca04bcd2bf3b882bcee5
Cristian Paul Castañeda <cristianpaul_c@yahoo.com>
2025-09-27 14:35:45 +0800

    Add settings page, configuration and daisy ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,13 +9,15 @@ import SettingsPage from './pages/SettingsPage';
 
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 
 import { Loader } from "lucide-react"; 
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
 
-  const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+  const {theme} = useThemeStore()
 
   useEffect(() => {
     checkAuth()
@@ -30,21 +32,23 @@ const App = () => {
   ) 
 
   return (
-    <div>
+
+      <div className="min-h-screen" data-theme={theme}>
+        
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={ authUser ? <HomePage/> : <Navigate to="/login"/> } />
+          <Route path="/signup" element={ !authUser ? <SignUpPage/> : <Navigate to="/" />} />
+          <Route path="/login" element={ !authUser ? <LoginPage/> : <Navigate to="/" /> } />
+          <Route path="/settings" element={ <SettingsPage/> } />
+          <Route path="/profile" element={ authUser ? <ProfilePage/> : <Navigate to="/login"/> } />
+        </Routes>
+
+        <Toaster />
       
-      <Navbar />
+      </div>
 
-      <Routes>
-        <Route path="/" element={ authUser ? <HomePage/> : <Navigate to="/login"/> } />
-        <Route path="/signup" element={ !authUser ? <SignUpPage/> : <Navigate to="/" />} />
-        <Route path="/login" element={ !authUser ? <LoginPage/> : <Navigate to="/" /> } />
-        <Route path="/settings" element={ <SettingsPage/> } />
-        <Route path="/profile" element={ authUser ? <ProfilePage/> : <Navigate to="/login"/> } />
-      </Routes>
-
-      <Toaster />
-    
-    </div>
   )
 }
 
